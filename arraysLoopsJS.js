70a5ed722e60d3db57c7c1028f714257ae095f35
Omar Salloum <omarsalloum08@gmail.com>
2025-03-28 01:12:55 +0100

    first tasks

---
+++ arraysLoopsJS.js
@@ -0,0 +1,29 @@
+// Exercise: create an array with three fruits
+const fruits = ["Apple", "Banana","Mango"];
+
+// Exercise: print the last element in the vegetables  array to the console
+const vegetables = ['Carrot', 'Tomato', 'Cabbage'];
+console.log(vegetables.pop());
+
+// Exercises:
+
+// const vegetables = ['Carrot', 'Tomato', 'Cabbage'];
+// print all elements of the vegetables  array to the console using 
+// the while loop
+
+// print all elements of the vegetables  array to the console using 
+// the for loop
+
+const vegetabless = ['Carrot', 'Tomato', 'Cabbage'];
+
+let i = 0;
+while (i < vegetabless.length) {
+    console.log(vegetabless[i]);
+    i++;
+}
+
+for (i = 0; i < vegetabless.length; i++) {
+    console.log(vegetabless[i]);
+}
+
+// HomeWork
